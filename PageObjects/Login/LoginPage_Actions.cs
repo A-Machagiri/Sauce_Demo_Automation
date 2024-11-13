c066a550dc4cd1be623bbdfb203b8fecfce43aa6
A-Machagiri <adunurimachagiri@gmail.com>
2024-11-13 15:44:24 +0530

    Create PageObjects/Login/LoginPage_Actions.cs

---
+++ PageObjects/Login/LoginPage_Actions.cs
@@ -0,0 +1,32 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace PageObjects.Login
+{
+    public partial class LoginPage
+    {
+        private IWebDriver _driver;
+        private WebDriverWait _wait;
+
+        public LoginPage(IWebDriver driver)
+        {
+            _driver = driver;
+            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+        }
+
+        public void EnterUsername(string username)
+        {
+            _driver.FindElement(_username).SendKeys(username);
+        }
+
+        public void EnterPassword(string password)
+        {
+            _driver.FindElement(_password).SendKeys(password);
+        }
+
+        public void ClickLoginButton()
+        {
+            _driver.FindElement(_loginButton).Click();
+        }
+    }
+}
